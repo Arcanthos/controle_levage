c2399e2c88f5581b657e50026b200d62fd0bc442
Louis <louis.barre.info@gmail.com>
2020-05-27 17:44:05 +0200

    création d'un formulaire de création d'utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,20 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\NewUserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/new-user", name="newUser")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
      */
-    public function nouvelleUtilisateur()
+    public function nouvelleUtilisateur(Request $request , EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
     {
+        // instanciation des différents éléments du formulaire
+        $user = new User();
+        $newUserForm = $this->createForm(NewUserFormType::class, $user);
+        $user->setIsActive(true);
+        $user->setRoles(['ROLE_CONTROLLER']);
 
+        // gestion de la requete  et de la persistance des données
+        $newUserForm->handleRequest($request);
+        if ($newUserForm->isSubmitted() && $newUserForm->isValid()){
+            // hashage du mot de passe saisi
+            $hashed = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashed);
+
+            //persistance des données dans la DB
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
 
         return $this->render('user/newUser.html.twig', [
             'controller_name' => 'UserController',
+            'newUserForm'=>$newUserForm->createView()
         ]);
     }
 
--- src/Form/NewUserFormType.php
+++ src/Form/NewUserFormType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class NewUserFormType extends AbstractType
@@ -12,14 +15,28 @@ class NewUserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('name')
-            ->add('firstname')
-            ->add('password')
-            ->add('email')
-            ->add('phone')
-            ->add('isActive')
-            ->add('roles')
+            ->add('username', TextType::class,[
+                'label'=> 'Pseudo'
+            ])
+            ->add('name', TextType::class,[
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom'
+            ])
+            ->add('email', TextType::class,[
+                'label'=> 'Adresse email'
+            ])
+            ->add('phone', TextType::class,[
+                'label'=> 'Téléphone'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type'=> PasswordType::class,
+                'invalid_message' => 'les deux mots de passe doivent être identique !',
+                'required'=> true,
+                'first_options'=> ['label'=>'Mot de passe'],
+                'second_options'=> ['label'=>'Répéter le mot passe']
+            ])
         ;
     }
 
