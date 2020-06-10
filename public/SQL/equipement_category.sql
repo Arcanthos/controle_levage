f3299ccd8e238dbc11c4461ba4d42924d08f61f7
Louis <louis.barre.info@gmail.com>
2020-06-10 14:36:30 +0200

    modification layouy bootstrap (gestion de pagdding) + ajour d'un script SQL pour catégorie de machine

---
+++ public/SQL/equipement_category.sql
@@ -0,0 +1,16 @@
+INSERT INTO equipment_category (category) VALUES ('GRUE AUXILIAIRE DE CHARGEMENT');
+INSERT INTO equipment_category (category) VALUES ('ACCESSOIRES POUR GRUE AUXILIAIRE');
+INSERT INTO equipment_category (category) VALUES ('BRAS DE LEVAGE POUR BENNES AMOVIBLES');
+INSERT INTO equipment_category (category) VALUES ('ELEVATEUR DE VEHICULES');
+INSERT INTO equipment_category (category) VALUES ('CRIC MOBILE / TABLE ELEVATRICE / GRUE D ATELIER');
+INSERT INTO equipment_category (category) VALUES ('HAYON INDUSTRIEL');
+INSERT INTO equipment_category (category) VALUES ('HAYON POUR PASSAGER');
+INSERT INTO equipment_category (category) VALUES ('CHARIOT ELEVATEUR / CHARGEUR FRONTAL');
+INSERT INTO equipment_category (category) VALUES ('PLATE-FORME ELEVATRICE MOBILE DE PERSONNEL');
+INSERT INTO equipment_category (category) VALUES ('B.O.M. ET COMPACTEUR');
+INSERT INTO equipment_category (category) VALUES ('PELLE HYDRAULIQUE');
+INSERT INTO equipment_category (category) VALUES ('VEHICULE SUR PISTE');
+INSERT INTO equipment_category (category) VALUES ('PORTE ET PORTAIL AUTOMATIQUE ET SEMI-AUTOMATIQUE');
+INSERT INTO equipment_category (category) VALUES ('PALANT / TREUIL');
+INSERT INTO equipment_category (category) VALUES ('CHARGEUSE PELLETEUSE');
+
