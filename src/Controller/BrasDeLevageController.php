35e1faf66bb0c96e7231d5eda6b1b13e7b34d8a8
Louis <louis.barre.info@gmail.com>
2020-06-16 13:16:52 +0200

    création des templates bras de levage + nomenclature

--- src/Controller/BrasDeLevageController.php
+++ src/Controller/BrasDeLevageController.php
@@ -8,11 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class BrasDeLevageController extends AbstractController
 {
     /**
-     * @Route("/bras/de/levage", name="bras_de_levage")
+     * @Route("control/bras-de-levage/periodicControl/{id}", name="bras_de_levagePeriodicControl")
      */
-    public function index()
+    public function controlePeriodique($id)
     {
-        return $this->render('control/bras_de_levage/index.html.twig', [
+        return $this->render('control/bras_de_levage/periodicControl.html.twig', [
+            'controller_name' => 'BrasDeLevageController',
+        ]);
+    }
+
+
+    /**
+     * @Route("control/bras-de-levage/CommissioningControl/{id}", name="bras_de_levageCommissioningControl")
+     */
+    public function controleMiseEnService($id)
+    {
+        return $this->render('control/bras_de_levage/commissioningControl.html.twig', [
+            'controller_name' => 'BrasDeLevageController',
+        ]);
+    }
+
+
+    /**
+     * @Route("control/bras-de-levage/ReturnToServiceControl/{id}", name="bras_de_levageReturnToServiceControl")
+     */
+    public function controleRemiseEnService($id)
+    {
+        return $this->render('control/bras_de_levage/returnToServiceControl.html.twig', [
             'controller_name' => 'BrasDeLevageController',
         ]);
     }
