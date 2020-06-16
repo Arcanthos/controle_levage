c38e0594d02b0772e285854cb8b04f5fcfb08d75
Fabienne DAVID <fabienne.david2019@campus-eni.fr>
2020-06-16 17:40:07 +0200

    Update du ControlType.php

--- src/Form/ControlType.php
+++ src/Form/ControlType.php
@@ -4,12 +4,8 @@ namespace App\Form;
 
 use App\Entity\Control;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ControlType extends AbstractType
@@ -19,42 +15,11 @@ class ControlType extends AbstractType
         $builder
             ->add('type', ChoiceType::class, [
                 'choices'=>[
-                    'commissioning'=>'commissioning',
-                    'periodic'=>'periodic',
-                    'puttingBackIntoService'=>'puttingBackIntoService',
+                    'Mise en service'=>'Mise en service',
+                    'Périodique'=>'Périodique',
+                    'Remise en service'=>'Remise en service',
                 ],
-                'choice_label' => function ($choice, $key, $value)
-                {
-                    if ('commissioning' === $choice) {
-                        return 'Mise en service';
-                    }
-                    elseif ('periodic' === $choice){
-                        return 'Périodique';
-                    }
-                    elseif ('puttingBackIntoService' === $choice)
-                    {
-                        return 'Remise en service';
-                    }
-                    return $key;
-                }
-
-                    ])
-
-            //Récupération des saisies de l'utilisateur
-           /* ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event)
-            {*/
-                /**@var Control $control*/
-                /*$control = $event->getData();
-
-                //le champ de la date du jour est rempli automatiquement lors de la validation du formulaire
-                $date = new \DateTime();
-                $control->setDate(clone $date);
-
-                //ainsi que la date du prochain contrôle dans 6 Mois
-                $interval = new \DateInterval('P6M');
-                $dateNextControl = $date->add($interval);
-                $control->setDateNextControl($dateNextControl);
-            })*/
+            ])
         ;
     }
 
