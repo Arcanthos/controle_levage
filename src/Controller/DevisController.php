b7caea9a9bc81bc55c284393df8a838336be732a
Louis <louis.barre.info@gmail.com>
2020-06-18 21:57:49 +0200

    génération de pdf pour le devis ok

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use App\Entity\Quote;
 use App\Repository\EquipmentRepository;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMException;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpKernel\KernelInterface;
 class DevisController extends AbstractController
 {
     /**
@@ -19,7 +22,7 @@ class DevisController extends AbstractController
      * @param EntityManager $entityManager
      * @return Response
      */
-    public function newDevis($equipmentID, EquipmentRepository $equipmentRepository, EntityManager $entityManager)
+    public function newDevis($equipmentID, EquipmentRepository $equipmentRepository, EntityManagerInterface $entityManager)
     {
         $equipmentToControl = $equipmentRepository->find($equipmentID);
         $newDevis = new Quote();
@@ -34,6 +37,39 @@ class DevisController extends AbstractController
         } catch (ORMException $e) {
             echo("ERREUR, le devis n'a pas pu être enregistré");
         }
+
+        //generation du pdf
+        //options du pdf
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        //instantiation d'un nouveau DOM pdf
+        $dompdf = new Dompdf($pdfOptions);
+
+        //retourner le fichier html dans le fichier twig
+        $html = $this->renderView('devis/devisPdf.html.twig', [
+            'title'=>'Devis'
+        ]);
+
+        //chargement du html dans le dompdf
+        $dompdf->loadHtml($html);
+
+        //parametre du dompdf
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Rendu du html en pdf
+        $dompdf->render();
+
+        // transformation des données du dompdf en données binaires
+        $output = $dompdf->output();
+
+        // In this case, we want to write the file in the public directory
+        $publicDirectory = $this->getParameter('kernel.project_dir') . '/public/PDF/devis';
+
+        $pdfFilepath =  $publicDirectory . '/'.$equipmentToControl->getModel().$equipmentToControl->getClientCompany()->getName().'.pdf';
+
+        // Write file to the desired path
+        file_put_contents($pdfFilepath, $output);
 
 
 
