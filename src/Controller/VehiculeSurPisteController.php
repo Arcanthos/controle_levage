d30d6f3ca51e1cacccc2727bb8e7c0f9326a2532
Fabienne DAVID <fabienne.david2019@campus-eni.fr>
2020-07-02 14:33:06 +0200

    Création du twig periodic et du contrôleur du véhicule sur piste Update du fichiers js

---
+++ src/Controller/VehiculeSurPisteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ControlRepository;
+use App\Repository\EquipmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehiculeSurPisteController extends AbstractController
+{
+    /**
+     * @Route("control/vehicule-sur-piste/periodicControl/{id}", name="vehicule_sur_pistePeriodicControl")
+     * @param $controlId
+     * @param $id
+     * @param ControlRepository $controlRepository
+     * @param EquipmentRepository $equipmentRepository
+     * @return Response
+     */
+    public function controlePeriodique($controlId, $id, ControlRepository $controlRepository, EquipmentRepository $equipmentRepository)
+    {
+        $equipment = $equipmentRepository->find($id);
+        $control = $controlRepository->find($controlId);
+        $user = $this ->getUser();
+
+        return $this->render('/control/vehicule-sur-piste/periodicControl.html.twig', [
+            'controller_name' => 'VehiculeSurPisteController',
+            'equipment'=>$equipment,
+            'control'=>$control,
+            'user'=>$user,
+        ]);
+    }
+}
